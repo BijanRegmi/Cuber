54d810f15d11be595cc0d4b2aa6bf666046a4a33
BijanRegmi <vizandabijan@gmail.com>
2021-03-23 23:01:33 +0545

    Timer running functions

---
+++ timer_fnxs.py
@@ -0,0 +1,40 @@
+import time
+
+class Time_calc():
+    def __init__(self):
+        self.state = 0          #0==>stopped    1==>running     -1==>paused
+        self.initial = 0
+        self.final = 0
+        self.elapsed = 0
+
+    def start(self):
+        self.state = 1
+        self.initial = time.time_ns()
+
+    def pause(self):
+        curr = time.time_ns()
+        if self.state == 1:
+            self.elapsed += curr - self.initial
+        elif self.state == -1:
+            self.initial = curr
+        self.state *= -1
+        return self.elapsed
+        
+    def reset(self):
+        self.state = 0
+        self.initial = 0
+        self.final = 0
+        self.elapsed = 0
+
+if __name__ == "__main__":
+    tim = Timer()
+
+    tim.start()
+    
+    time.sleep(3)
+    
+    el = tim.pause()
+    
+    tim.reset()
+    
+    print(el)
